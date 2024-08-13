b3f2a27b76dc8e1d54f467e390b60db7f4b139ad
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-08-13 23:50:15 +0900

    [Section 4] Users Microservice - welcome() 메서드

--- section 4/user-service/src/main/java/chocola/user_service/controller/UserController.java
+++ section 4/user-service/src/main/java/chocola/user_service/controller/UserController.java
@@ -1,13 +1,22 @@
 package chocola.user_service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @Value("${greeting.message}")
+    private String greetingMessage;
+
     @GetMapping("/health_check")
     public String status() {
         return "It's working in User-Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return this.greetingMessage;
     }
 }
